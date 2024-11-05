2571d641e6b1b81326d80131fe45156f4a10243b
sarfaraz-baloch <sarfarazkohda321@gmail.com>
2024-11-05 15:59:26 +0500

    fixed backend bugs

--- src/lib/db/ConnectDb.js
+++ src/lib/db/ConnectDb.js
@@ -1,27 +1,14 @@
-// import mongoose from "mongoose";
-// export async function ConnectDb() {
-//     try {
-//         let connection ;
-//         connection = await mongoose.connect(process.env.MONGODB_URL);
-//         console.log("Database connected")
-//         return connection
-//     }
-//     catch(err){
-//         console.log(" Database not connected",err)
-//     }   
-// }
+import mongoose from "mongoose";
+export async function ConnectDb() {
+    try {
+        let connection ;
+        connection = await mongoose.connect(process.env.MONGODB_URL);
+        console.log("Database connected")
+        return connection
+    }
+    catch(err){
+        console.log(" Database not connected",err)
+    }   
+}
 
-import mongoose from 'mongoose';
 
-const connectDb = async () => {
-  if (mongoose.connections[0].readyState) {
-    // Already connected
-    return;
-  }
-  await mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000,  // Timeout after 5 seconds
-  });
-};
-export default connectDb;
